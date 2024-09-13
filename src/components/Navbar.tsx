0d8db60e0332ffef64043c3667d550e436924e5a
Muskan Markam <shilpirajnita@gmail.com>
2024-09-13 08:12:50 +0530

    removed 's from MUSKNAS CAFE because ' was causing problems while deploying

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       </div>
       {/* LOGO */}
       <div className="text-xl md:font-bold flex-1 md:text-center">
-        <Link href="/">MUSKAN'S CAFE</Link>
+        <Link href="/">MUSKANS CAFE</Link>
       </div>
       {/* MOBILE MENU */}
       <div className="md:hidden">
