c84778e34388fe56108e76faf13175bb5256a1b0
Muskan Markam <shilpirajnita@gmail.com>
2024-09-13 08:13:22 +0530

    added &#39 to replicate ' in MUSKANS CAFE

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
-      <Link href="/" className="font-bold text-xl">MUSKANS CAFE</Link>
+      <Link href="/" className="font-bold text-xl">MUSKAN&#39;S CAFE</Link>
       <p>© ALL RIGHTS RESERVED.</p> 
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       </div>
       {/* LOGO */}
       <div className="text-xl md:font-bold flex-1 md:text-center">
-        <Link href="/">MUSKANS CAFE</Link>
+        <Link href="/">MUSKAN&#39;S CAFE</Link>
       </div>
       {/* MOBILE MENU */}
       <div className="md:hidden">
